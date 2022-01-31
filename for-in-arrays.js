3801a5eaedecd8034a9576fd352d2c845464272b
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-01-30 21:13:03 -0700

    Loops

---
+++ for-in-arrays.js
@@ -0,0 +1,9 @@
+let total = 0;
+let amounts= [1,2,3,4];
+for (let amount of amounts) {
+  total += amount;
+}
+console.log('Order total is: ', total);
+let text='hello world';
+for (let character of text)
+console.log(character);
---
+++ haha-script.js
@@ -0,0 +1,17 @@
+
+var laugh = function (number)
+
+{var text='';
+    for (i=0;i<=number;i++)
+    
+    {
+     text+='ha';
+        
+    }
+    return text;
+    
+}
+
+/* finish the function expression */
+
+console.log(laugh(2));
